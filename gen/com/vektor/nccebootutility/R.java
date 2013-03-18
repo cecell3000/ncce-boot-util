3ddd8592b8a707d6c4e75c283c925535db9d193d
Zevektor <clio2k6@gmail.com>
2013-03-18 17:28:51 +0100

    Improved Layout and added warning.

--- gen/com/vektor/nccebootutility/R.java
+++ gen/com/vektor/nccebootutility/R.java
@@ -28,6 +28,7 @@ public final class R {
         public static final int button2=0x7f080002;
         public static final int button3=0x7f080003;
         public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080004;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
@@ -41,6 +42,7 @@ public final class R {
         public static final int poweroff=0x7f050003;
         public static final int reboot=0x7f050002;
         public static final int recovery=0x7f050004;
+        public static final int warning=0x7f050005;
     }
     public static final class style {
         /** 
